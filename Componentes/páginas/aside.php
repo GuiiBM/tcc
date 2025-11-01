dc94c7cdef49085f7c6dddd7c97024dbab6b002a
GuiiBM <gbmubatuba@gmail.com>
2025-10-31 21:30:15 -0300

    v-35

--- Componentes/páginas/aside.php
+++ Componentes/páginas/aside.php
@@ -1,73 +1,13 @@
 <aside>
     <div class="aside-title">
-        Playlist
+        Propaganda
     </div>
     <div class="aside-container">
     <ul class="aside-ul">
         <li>
          <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
-         </a>
-        </li>
-        <li>
-         <a href="#">
-         <div style="font-weight:bold; font-size:1.1rem;" href="#">Música: Propaganda</div>
-         <div href="#">Artista: Onze</div>
+         <div style="font-weight:bold; font-size:1.1rem;" href="#">Propaganda</div>
+         <div href="#">Paga</div>
          </a>
         </li>
     </ul>
